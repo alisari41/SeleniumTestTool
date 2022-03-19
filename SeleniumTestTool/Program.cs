f7ef4e75cda0eef0459563780739cd53c3b3bc66
alisari41 <alisari41@outlook.com>
2022-03-19 23:23:18 +0300

    Add project files.

---
+++ SeleniumTestTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeleniumTestTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Web tabanlı sistemlerde testleri yapmak için geliştirilmiş bir test aracıdır veya kütüphanedir.
+            
+        }
+    }
+}
