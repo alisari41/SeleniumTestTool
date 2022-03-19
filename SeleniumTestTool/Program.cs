6c5e696bc50a20412e3e59eea093b1f2693f2c2f
alisari41 <alisari41@outlook.com>
2022-03-20 02:53:44 +0300

    n11 Selenium ile sepete ürün ekleme işlemi

--- SeleniumTestTool/Program.cs
+++ SeleniumTestTool/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 
 namespace SeleniumTestTool
 {
@@ -6,7 +10,72 @@ namespace SeleniumTestTool
     {
         static void Main(string[] args)
         {//Web tabanlı sistemlerde testleri yapmak için geliştirilmiş bir test aracıdır veya kütüphanedir.
+
+            IWebDriver driver = new FirefoxDriver();//Hangi tarayıcı üzerinden işlem yapacağımı belirtiyorum.
+            string link = @"https://www.n11.com/";
+            driver.Navigate().GoToUrl(link);//Url'e gitme işlemi
+
+
+
+            //{//Login Test Aşaması
+            //    //Giriş yap butonunun class=btnSignIn bilgisi
+            //    driver.FindElement(By.ClassName("btnSignIn")).Click(); // Butonu clickleyip o sayfaya gidecek
+
+            //    //Email textbox'ın id=email bilgisi
+            //    driver.FindElement(By.Id("email")).SendKeys("alisari41@outlook.com");// SendKesys() komudu ile değer gönderilecek
+
+            //    //Şifre textbox'ın id=password bilgisi
+            //    driver.FindElement(By.Id("password")).SendKeys("123456*");// SendKesys() komudu ile değer gönderilecek
+
+            //    // Üye girişi yapmak için butonun id=loginButton bilgisi
+            //    driver.FindElement(By.Id("loginButton")).Click();// Id bilgisi varsa onu kullanalım class bilgisi birden çok yerde kullanılabiliniyor.
+            //}
+
+
+            // Arama textbox'ın      id=searchData   bilgisi
+            driver.FindElement(By.Id("searchData")).SendKeys("Samsung"); // Samsung verilerini arıyorum
+
+            // Arama Butonun      class=searchBtn  bilgisi
+            driver.FindElement(By.ClassName("searchBtn")).Click();
             
+
+            // Normalde sadece class bilgisi varsa onu verebilirim fakat bazen sayfalarımızda class veya id bilgisi verilmeye bilir o yüzden adım adım bakıyorum. Alt div'e nasıl geçilir
+            // 2. sayfaya geçmek için öncelikle tüm sayfanın olduğu div lazım   id=contentListing    bilgisi 
+            // Sonrasında altında bir div var o divin içersine giriyoruz
+            // Tekrar bakıyoruz kaç div var tek div olduğu için 
+            // İçinde 2 farklı div var biz sağdaki divi alıcaz. Birden fazla olduğu için [2] dizi şeklinde hangisi ise seçmeliyiz şeklinde devam ederiz
+
+            // Veya bu adımları yapmak yerine  direk istediğimiz mesela ikinci sayfa butonunu inceleyerek Copy XPath yapabliriz
+            // fakat "contentListing" yerine 'contentListing' yazılması gerekir yani tek tırnak
+            
+            // SAYFA NUMARASI 2 OLAN BUTON'UN XPATH'I LAZIM
+            //                          bütün sayfa içersinde id=contentListing bul altındaki div'e gir
+            driver.FindElement(By.XPath("//*[@id='contentListing']/div/div/div[1]/div[6]/a[2]")).Click(); // XPath bütün sayfa içinde arama yapan bir cümlecik
+
+
+            // Rasgele bir ürün seçiyorum o ürüne ait en baştaki div için id=p-530088692 bilgisi a link bilgisinin XPath olarak kopyalarsam unutmayayım Tek Tırnak ve arada boşluk yok visual kendi otomatik atıyor düzelt
+            driver.FindElement(By.XPath("//*[@id='p-489569060']/div[1]/a")).Click();
+            Thread.Sleep(9000);//Bekleme süresi koyuyyorum sayfa yüklensin yoksa patlıyor
+
+
+            // Detayları görmek için class=detailPreviewBtn
+            driver.FindElement(By.ClassName("detailPreviewBtn")).Click();
+
+            // Detayları kapatmak için class=btnOk
+            driver.FindElement(By.ClassName("btnOk")).Click();
+
+            Thread.Sleep(2000);
+
+            // Adetini 1 tane arttırmak için buton bilgisi XPath hali
+            driver.FindElement(By.XPath("/html/body/div[1]/div[3]/div/div[2]/div[1]/div[1]/div[2]/form/div[2]/div[2]/div[1]/div/div/span[1]")).Click();
+
+            // Sepete eklemek için buton bilgisi class=addBasketUnify
+            driver.FindElement(By.ClassName("addBasketUnify")).Click();
+
+
+
+
+
         }
     }
 }
