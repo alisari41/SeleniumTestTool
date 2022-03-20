cb671240ef1c6193c824d21bbf8446a6f38a95df
alisari41 <alisari41@outlook.com>
2022-03-20 14:29:13 +0300

    Sepete ekle sil testleri yapıldı

--- SeleniumTestTool/Program.cs
+++ SeleniumTestTool/Program.cs
@@ -17,27 +17,28 @@ namespace SeleniumTestTool
 
 
 
-            //{//Login Test Aşaması
-            //    //Giriş yap butonunun class=btnSignIn bilgisi
-            //    driver.FindElement(By.ClassName("btnSignIn")).Click(); // Butonu clickleyip o sayfaya gidecek
+            {//Login Test Aşaması
+                //Giriş yap butonunun class=btnSignIn bilgisi
+                driver.FindElement(By.ClassName("btnSignIn")).Click(); // Butonu clickleyip o sayfaya gidecek
 
-            //    //Email textbox'ın id=email bilgisi
-            //    driver.FindElement(By.Id("email")).SendKeys("alisari41@outlook.com");// SendKesys() komudu ile değer gönderilecek
+                //Email textbox'ın id=email bilgisi
+                driver.FindElement(By.Id("email")).SendKeys("alisarideneme@outlook.com");// SendKesys() komudu ile değer gönderilecek
 
-            //    //Şifre textbox'ın id=password bilgisi
-            //    driver.FindElement(By.Id("password")).SendKeys("123456*");// SendKesys() komudu ile değer gönderilecek
+                //Şifre textbox'ın id=password bilgisi
+                driver.FindElement(By.Id("password")).SendKeys("258963ali");// SendKesys() komudu ile değer gönderilecek
 
-            //    // Üye girişi yapmak için butonun id=loginButton bilgisi
-            //    driver.FindElement(By.Id("loginButton")).Click();// Id bilgisi varsa onu kullanalım class bilgisi birden çok yerde kullanılabiliniyor.
-            //}
+                // Üye girişi yapmak için butonun id=loginButton bilgisi
+                driver.FindElement(By.Id("loginButton")).Click();// Id bilgisi varsa onu kullanalım class bilgisi birden çok yerde kullanılabiliniyor.
+            }
 
+            Thread.Sleep(2000);
 
             // Arama textbox'ın      id=searchData   bilgisi
             driver.FindElement(By.Id("searchData")).SendKeys("Samsung"); // Samsung verilerini arıyorum
 
             // Arama Butonun      class=searchBtn  bilgisi
             driver.FindElement(By.ClassName("searchBtn")).Click();
-            
+
 
             // Normalde sadece class bilgisi varsa onu verebilirim fakat bazen sayfalarımızda class veya id bilgisi verilmeye bilir o yüzden adım adım bakıyorum. Alt div'e nasıl geçilir
             // 2. sayfaya geçmek için öncelikle tüm sayfanın olduğu div lazım   id=contentListing    bilgisi 
@@ -47,15 +48,16 @@ namespace SeleniumTestTool
 
             // Veya bu adımları yapmak yerine  direk istediğimiz mesela ikinci sayfa butonunu inceleyerek Copy XPath yapabliriz
             // fakat "contentListing" yerine 'contentListing' yazılması gerekir yani tek tırnak
-            
+
             // SAYFA NUMARASI 2 OLAN BUTON'UN XPATH'I LAZIM
+            //*[@id="contentListing"]/div/div/div[2]/div[4]/a[2]
             //                          bütün sayfa içersinde id=contentListing bul altındaki div'e gir
-            driver.FindElement(By.XPath("//*[@id='contentListing']/div/div/div[1]/div[6]/a[2]")).Click(); // XPath bütün sayfa içinde arama yapan bir cümlecik
+            driver.FindElement(By.XPath("//*[@id='contentListing']/div/div/div[2]/div[4]/a[2]")).Click(); // XPath bütün sayfa içinde arama yapan bir cümlecik
 
 
             // Rasgele bir ürün seçiyorum o ürüne ait en baştaki div için id=p-530088692 bilgisi a link bilgisinin XPath olarak kopyalarsam unutmayayım Tek Tırnak ve arada boşluk yok visual kendi otomatik atıyor düzelt
             driver.FindElement(By.XPath("//*[@id='p-489569060']/div[1]/a")).Click();
-            Thread.Sleep(9000);//Bekleme süresi koyuyyorum sayfa yüklensin yoksa patlıyor
+            Thread.Sleep(4000);//Bekleme süresi koyuyyorum sayfa yüklensin yoksa patlıyor
 
 
             // Detayları görmek için class=detailPreviewBtn
@@ -72,9 +74,43 @@ namespace SeleniumTestTool
             // Sepete eklemek için buton bilgisi class=addBasketUnify
             driver.FindElement(By.ClassName("addBasketUnify")).Click();
 
+            Thread.Sleep(1000);
+
+            // Sepette adeti olmayan ürün hata mesajını kapatma XPath
+            driver.FindElement(By.ClassName("confirm")).Click();
+
+            Thread.Sleep(1000);
+
+            // Sepete eklediğim ürün adetini azaltma class=spinnerDown 
+            driver.FindElement(By.ClassName("spinnerDown")).Click();
+
+            // Sepete eklemek için buton bilgisi class=addBasketUnify
+            driver.FindElement(By.ClassName("addBasketUnify")).Click();
+
+            Thread.Sleep(2000);
+
+            // Sepete Gitmek için buton bilgisi class=btnGoBasket
+            driver.FindElement(By.ClassName("btnGoBasket")).Click();
 
 
+            // Karşıma çıkan Aydıtlatma Metnini kapatmak için XPath
+            // Aydınlatma metni kullanıcı giriş yapmadıysa çıkar
+            //driver.FindElement(By.XPath("/html/body/div[4]/div/div[2]/span")).Click();
+        
+            Thread.Sleep(2000);
 
+            // Sepetteki Ürünü silmek için class=removeProd 
+            driver.FindElement(By.ClassName("removeProd")).Click();
+
+            Thread.Sleep(2000);
+
+            // Anasayfaya Dönmek için class=home 
+            driver.FindElement(By.XPath("/html/body/div[1]/header/div/a")).Click();
+
+            Thread.Sleep(9000);
+
+            // En son kullanıcı hesabımdan çıkış yapmak için class=logoutBtn
+            driver.FindElement(By.ClassName("logoutBtn")).Click();
 
         }
     }
